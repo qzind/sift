f4f729eaffd86e3beda3eecbaa8b92627df5b2b0
Berenz <thebrettberenz@gmail.com>
2016-08-17 12:08:36 -0400

    Rework api to static calls

--- sift.js
+++ sift.js
@@ -94,13 +94,13 @@ var Sifter = (function() {
             new _Driver('TEXTONLY.PPD', _OS.MAC, _Types.RAW, true),
 
             /** Mac Dual-Mode Printers */
-            new Driver('TM-T88V.PPD', _OS.MAC, _Types.BOTH, true),
+            new _Driver('TM-T88V.PPD', _OS.MAC, _Types.BOTH, true),
 
             /** Linux Raw-Only Printers */
             new _Driver('TEXTONLY.PPD', _OS.LINUX, _Types.RAW, true),
 
             /** Linux Dual-Mode Printers */
-            new Driver('EPTMBATH.PPD', _OS.LINUX, _Types.BOTH, true),
+            new _Driver('EPTMBATH.PPD', _OS.LINUX, _Types.BOTH, true),
         ],
 
         usbVendors: [
@@ -126,7 +126,7 @@ var Sifter = (function() {
             return _sift.printDrivers[0];
         },
 
-        findUsbVendor: function() {
+        findUsbVendor: function(vendorId, productId) {
             //TODO
         },
 
@@ -185,82 +185,8 @@ var Sifter = (function() {
 
             //look for usb style filters
             usb: function(list, filter) {
-                //TODO
+                //TODO - name, type, vendor, product, device, endpoint
             }
-        },
-
-        /** Parses scale reading from USB raw output */
-        parseScaleData: function(data) {
-            var weight = {
-                raw: null, value: null, toString: function() {
-                    return weight.value;
-                },
-                units: {
-                    raw: null, value: null, toString: function() {
-                        return weight.units.value;
-                    }
-                },
-                status: {
-                    raw: null, value: null, toString: function() {
-                        return weight.status.value;
-                    }
-                },
-                precision: null,
-            };
-
-            // Filter erroneous data
-            if (data.length < 4 || data.slice(2, 8).join('') === '000000000000') {
-                return weight;
-            }
-
-            // Get status
-            weight.status.raw = parseInt(data[1], 16);
-            switch(weight.status.raw) {
-                case 1: // fault
-                case 5: // underweight
-                case 6: // overweight
-                case 7: // calibrate
-                case 8: // re-zero
-                    weight.status.value = 'Error';
-                    break;
-                case 3: // busy
-                    weight.status.value = 'Busy';
-                    break;
-                case 2: // stable at zero
-                case 4: // stable non-zero
-                default:
-                    weight.status.value = 'Stable';
-            }
-
-            // Get precision
-            weight.precision = parseInt(data[3], 16);
-            weight.precision = weight.precision ^ -256; //unsigned to signed
-            weight.precision = weight.precision == -256? 0:weight.precision; //xor on 0 causes issues
-
-            // Get units
-            weight.units.raw = parseInt(data[2], 16);
-            switch(weight.units.raw) {
-                case 2:
-                    weight.units.value = 'g';
-                    break;
-                case 3:
-                    weight.units.value = 'kg';
-                    break;
-                case 11:
-                    weight.units.value = 'oz';
-                    break;
-                case 12:
-                default:
-                    weight.units.value = 'lbs';
-            }
-            // Get weight
-            data.splice(0, 4);
-            data.reverse();
-            weight.raw = parseInt(data.join(''), 16);
-            weight.raw *= Math.pow(10, weight.precision);
-            weight.raw = weight.toFixed(Math.abs(weight.precision));
-            weight.value = weight + weight.units + ' - ' + status;
-            return weight;
         }
 
     };
@@ -268,99 +194,163 @@ var Sifter = (function() {
     ///// PUBLIC DATA MAPPING /////
 
     // TODO: jsDocs for the Sifter class
-    function Sifter(format, list) {
-        var _debug = true;
+    var sift = {
 
-        //setup variables used on a per-instance basis
-        Object.defineProperties(this, {
-            //constants
-            "format": { value: format, enumerable: true },
+        /**
+         * Will filter out only items that match specified filter.
+         * TODO - docs
+         *
+         * @param list
+         * @param filters
+         * @returns {Array} The filtered list
+         */
+        toss: function(list, filters) {
+            if (!list || list.length == 0) { return list; }
+            var alter = list.slice();
 
-            //accessor
-            "debug": { value: _debug, enumerable: true, writable: true },
+            if (alter[0].driver !== undefined) {
+                _sift.filter.printers(alter, filters);
+            } else if (alter[0].vendor !== undefined) {
+                _sift.filter.usb(alter, filters);
+            } else {
+                throw new Error("Cannot determine list's element type");
+            }
 
-            //data
-            "originalList": { value: list },
-            "filteredList": { value: list.slice(), enumerable: true, writable: true } //copy list to separate reference
-        });
-    }
+            return alter;
+        },
+
+        /**
+         * Will filter out any items that do not match the specified filter.
+         * TODO - docs
+         *
+         * @param list
+         * @param filters
+         * @returns {Array} The filtered list
+         */
+        keep: function(list, filters) {
+            //to keep these filters we need to toss everything that isn't, so flip all the filters and call .toss()
+            for(var key in filters) {
+                if (filters.hasOwnProperty(key)) {
+                    if (typeof filters[key] === 'boolean') {
+                        filters[key] = !filters[key];
+                    } else if (typeof filters[key] === 'string') {
+                        filters[key] = '^((?!' + filters[key] + ').)*$'; //strings used as regex, flip to negative lookahead
+                    } else {
+                        filters[key] = ~filters[key];
+                    }
+                }
+            }
+
+            return sift.toss(list, filters);
+        },
+
+        parse: {
+            /**
+             * Parses scale reading from USB raw output
+             *
+             * @param data
+             * @returns
+             *
+             * @memberOf sift.parse
+             */
+            scale: function(data) {
+                var scale = {
+                    status: { raw: 0x00, value: '' },
+                    precision: { raw: 0x00, value: 0 },
+                    units: { raw: 0x00, value: '' },
+                    weight: { raw: 0x00, value: 0 },
+                    toString: function() {
+                        return this.weight.value + this.units.value + ' - ' + this.status.value;
+                    }
+                };
 
 
-    //Setup constants shared across all instances
-    Object.defineProperties(Sifter, {
-        "PRINT": { value: 0 },
-        "USB": { value: 1 },
+                // Filter erroneous data
+                if (data.length < 4 || data.slice(2, 8).join('') === '000000000000') {
+                    return scale;
+                }
 
+                // Get status
+                scale.status.raw = parseInt(data[1], 16);
+                switch(scale.status.raw) {
+                    case 1: // fault
+                        scale.status.value = 'Fault';
+                        break;
+                    case 2: // stable at zero
+                        scale.status.value = 'Stable (Zero)';
+                        break;
+                    case 3: // busy
+                        scale.status.value = 'Busy';
+                        break;
+                    case 4: // stable non-zero
+                        scale.status.value = 'Stable';
+                        break;
+                    case 5: // underweight
+                        scale.status.value = 'Underweight';
+                        break;
+                    case 6: // overweight
+                        scale.status.value = 'Overweight';
+                        break;
+                    case 7: // calibrate
+                        scale.status.value = 'Calibrate';
+                        break;
+                    case 8: // re-zero
+                        scale.status.value = 'Tare';
+                        break;
+                    default:
+                        scale.status.value = 'Unknown';
+                        break;
+                }
+
+                // Get precision
+                scale.precision.raw = parseInt(data[3], 16);
+                scale.precision.value = scale.precision.raw ^ -256; //unsigned to signed
+                scale.precision.value = scale.precision.value == -256? 0:scale.precision.value; //xor on 0 causes issues
+
+                // Get units
+                scale.units.raw = parseInt(data[2], 16);
+                switch(scale.units.raw) {
+                    case 2:
+                        scale.units.value = 'g';
+                        break;
+                    case 3:
+                        scale.units.value = 'kg';
+                        break;
+                    case 11:
+                        scale.units.value = 'oz';
+                        break;
+                    case 12:
+                    default:
+                        scale.units.value = 'lbs';
+                }
+
+                // Get weight
+                var wData = data.slice(4).reverse();
+                scale.weight.raw = parseInt(wData.join(''), 16);
+                scale.weight.value *= Math.pow(10, scale.precision.value);
+                scale.weight.value = scale.weight.toFixed(Math.abs(scale.precision.value));
+
+                return scale;
+            }
+        },
+
+        /**
+         * Turn on or off sift debugging statements in the console.
+         *
+         * @param {boolean} active Whether debugging is enabled
+         */
+        debug: function(active) {
+            _sift.debug = active;
+        }
+    };
+
+    //Setup constants
+    Object.defineProperties(sift, {
         "Type": { value: _Types },
         "OS": { value: _OS }
     });
 
-
-    //Setup functions shared across all instances
-
-    /**
-     * Will filter out only items that match specified filter.
-     *
-     * @param filters TODO - docs
-     * @returns {Sifter} Reference to self to allow chaining calls.
-     */
-    Sifter.prototype.toss = function(filters) {
-        //noinspection FallThroughInSwitchStatementJS
-        switch(this.format) {
-            default:
-                console.warn("Using unknown type '" + this.format + "' on Sifter instance. Defaulting to PRINT");
-            case Sifter.PRINT:
-                _sift.filter.printers(this.filteredList, filters);
-                break;
-            case Sifter.USB:
-                _sift.filter.usb(this.filteredList, filters);
-                break;
-        }
-
-        return this;
-    };
-
-    /**
-     * Will filter out any items that do not match the specified filter.
-     *
-     * @param filters TODO - docs
-     * @returns {Sifter} Reference to self to allow chaining calls.
-     */
-    Sifter.prototype.keep = function(filters) {
-        //to keep these filters we need to toss everything that isn't, so flip all the filters and call .toss()
-        for(var key in filters) {
-            if (filters.hasOwnProperty(key)) {
-                if (typeof filters[key] === 'boolean') {
-                    filters[key] = !filters[key];
-                } else if (typeof filters[key] === 'string') {
-                    filters[key] = '^((?!'+filters[key]+').)*$'; //strings used as regex, flip to negative lookahead
-                } else {
-                    filters[key] = ~filters[key];
-                }
-            }
-        }
-
-        return this.toss(filters);
-    };
-
-    /**
-     * @returns {Array} Returned the list as filtered by any <code>keep</code> or <code>toss</code> calls.
-     */
-    Sifter.prototype.list = function() {
-        return this.filteredList;
-    };
-
-    /**
-     * Restore the Sifter's list back to its original collection.
-     * @returns {Sifter} Reference to self to allow chaining calls.
-     */
-    Sifter.prototype.reset = function() {
-        this.filteredList = this.originalList.slice();
-        return this;
-    };
-
-
-    return Sifter;
+    return sift;
 
 })();
 
--- tests/test-printer.js
+++ tests/test-printer.js
@@ -71,47 +71,42 @@ var TestSuite = {
         /** Test results on printer formatted lists */
         printer: function() {
             //test result filtering flags - active if entry should be removed after filter call
-            var NONE = 0, REMOVE_VIRTUAL = 1, REMOVE_PHYSICAL = 2, REMOVE_PIXEL = 4, REMOVE_RAW = 8, REMOVE_BOTH = 16, REMOVE_NAME = 32, REMOVE_UNNAME = 64;
+            var REMOVE_VIRTUAL = 1, REMOVE_PHYSICAL = 2, REMOVE_PIXEL = 4, REMOVE_RAW = 8, REMOVE_BOTH = 16, REMOVE_NAME = 32, REMOVE_UNNAME = 64;
 
             var testData = [
                 { name: 'HP Color LaserJet 2500', driver: 'HP Color LaserJet 2500 PS Class Driver', test: REMOVE_PHYSICAL | REMOVE_PIXEL | REMOVE_UNNAME },
                 { name: 'PDFwriter', driver: '/private/etc/cups/ppd/PDFwriter.ppd', test: REMOVE_VIRTUAL | REMOVE_PIXEL | REMOVE_NAME },
                 { name: 'ZDesigner LP2844', driver: 'ZDesigner LP 2844.ppd', test: REMOVE_PHYSICAL | REMOVE_BOTH | REMOVE_UNNAME },
                 { name: 'Epson TM88 IV', driver: 'Generic / Text Only', test: REMOVE_PHYSICAL | REMOVE_RAW | REMOVE_UNNAME },
-                { name: 'PDF', driver: 'PDF.ppd', test: REMOVE_VIRTUAL | REMOVE_PIXEL | REMOVE_NAME }
+                { name: 'PDF', driver: 'CUPS-PDF.PPD', test: REMOVE_VIRTUAL | REMOVE_PIXEL | REMOVE_NAME }
             ];
 
-            var siftPrinters = new Sifter(Sifter.PRINT, testData);
-            siftPrinters.debug = true;
-            // siftPrinters._dump();
+            sift.debug(true);
 
             TestSuite.stepper.original = testData;
-            TestSuite.stepper.checkStep('Initial setup', siftPrinters.list(), NONE);
 
-            TestSuite.stepper.checkStep('Remove Virtual File Printers', siftPrinters.reset().toss({ physical: false }).list(), REMOVE_VIRTUAL);
-            TestSuite.stepper.checkStep('Keep Virtual File Printers', siftPrinters.reset().keep({ physical: false }).list(), REMOVE_PHYSICAL);
+            TestSuite.stepper.checkStep('Remove Virtual File Printers', sift.toss(testData, { physical: false }), REMOVE_VIRTUAL);
+            TestSuite.stepper.checkStep('Keep Virtual File Printers', sift.keep(testData, { physical: false }), REMOVE_PHYSICAL);
 
-            TestSuite.stepper.checkStep('Remove Physical Printers', siftPrinters.reset().toss({ physical: true }).list(), REMOVE_PHYSICAL);
-            TestSuite.stepper.checkStep('Keep Physical Printers', siftPrinters.reset().keep({ physical: true }).list(), REMOVE_VIRTUAL);
+            TestSuite.stepper.checkStep('Remove Physical Printers', sift.toss(testData, { physical: true }), REMOVE_PHYSICAL);
+            TestSuite.stepper.checkStep('Keep Physical Printers', sift.keep(testData, { physical: true }), REMOVE_VIRTUAL);
 
-            TestSuite.stepper.checkStep('Remove Pixel Only Printers', siftPrinters.reset().toss({ type: Sifter.Type.PIXEL }).list(), REMOVE_PIXEL);
-            TestSuite.stepper.checkStep('Keep Pixel Only Printers', siftPrinters.reset().keep({ type: Sifter.Type.PIXEL }).list(), REMOVE_RAW | REMOVE_BOTH);
+            TestSuite.stepper.checkStep('Remove Pixel Only Printers', sift.toss(testData, { type: sift.Type.PIXEL }), REMOVE_PIXEL);
+            TestSuite.stepper.checkStep('Keep Pixel Only Printers', sift.keep(testData, { type: sift.Type.PIXEL }), REMOVE_RAW | REMOVE_BOTH);
 
-            TestSuite.stepper.checkStep('Remove Raw Only Printers', siftPrinters.reset().toss({ type: Sifter.Type.RAW }).list(), REMOVE_RAW);
-            TestSuite.stepper.checkStep('Keep Raw Only Printers', siftPrinters.reset().keep({ type: Sifter.Type.RAW }).list(), REMOVE_BOTH | REMOVE_PIXEL);
+            TestSuite.stepper.checkStep('Remove Raw Only Printers', sift.toss(testData, { type: sift.Type.RAW }), REMOVE_RAW);
+            TestSuite.stepper.checkStep('Keep Raw Only Printers', sift.keep(testData, { type: sift.Type.RAW }), REMOVE_BOTH | REMOVE_PIXEL);
 
-            TestSuite.stepper.checkStep('Remove Dual Only Printers', siftPrinters.reset().toss({ type: Sifter.Type.BOTH }).list(), REMOVE_BOTH);
-            TestSuite.stepper.checkStep('Keep Dual Only Printers', siftPrinters.reset().keep({ type: Sifter.Type.BOTH }).list(), REMOVE_PIXEL | REMOVE_RAW);
+            TestSuite.stepper.checkStep('Remove Dual Only Printers', sift.toss(testData, { type: sift.Type.BOTH }), REMOVE_BOTH);
+            TestSuite.stepper.checkStep('Keep Dual Only Printers', sift.keep(testData, { type: sift.Type.BOTH }), REMOVE_PIXEL | REMOVE_RAW);
 
-            TestSuite.stepper.checkStep('Remove "PDF" Printers', siftPrinters.reset().toss({ name: "PDF" }).list(), REMOVE_NAME);
-            TestSuite.stepper.checkStep('Keep "PDF" Printers', siftPrinters.reset().keep({ name: "PDF" }).list(), REMOVE_UNNAME);
+            TestSuite.stepper.checkStep('Remove "PDF" Printers', sift.toss(testData, { name: "PDF" }), REMOVE_NAME);
+            TestSuite.stepper.checkStep('Keep "PDF" Printers', sift.keep(testData, { name: "PDF" }), REMOVE_UNNAME);
 
             //TODO ??
-            // TestSuite.stepper.checkStep('Remove Printers', siftPrinters.reset().toss({ os: Sifter.OS.MAC }).list());
-            // TestSuite.stepper.checkStep('Remove Printers', siftPrinters.reset().toss({ os: Sifter.OS.LINUX }).list());
-            // TestSuite.stepper.checkStep('Remove Printers', siftPrinters.reset().toss({ os: Sifter.OS.WINDOWS }).list());
-
-            TestSuite.stepper.checkStep('Final Clear', siftPrinters.reset().list(), NONE);
+            // TestSuite.stepper.checkStep('Remove Printers', sift.toss({ os: sift.OS.MAC }));
+            // TestSuite.stepper.checkStep('Remove Printers', sift.toss({ os: sift.OS.LINUX }));
+            // TestSuite.stepper.checkStep('Remove Printers', sift.toss({ os: sift.OS.WINDOWS }));
         },
 
         /** Test results on usb formatted lists */
