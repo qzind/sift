668787acc648485827e9cd115d8ddde002b382cd
Berenz <thebrettberenz@gmail.com>
2016-08-22 12:13:19 -0400

    Rename private vars and simplify scale status

--- sift.js
+++ sift.js
@@ -12,7 +12,7 @@ var Sifter = (function() {
     ///// PRIVATE DATA MAPPING /////
 
     //printer driver mapping
-    function _Driver(name, os, type, physical) {
+    function Driver(name, os, type, physical) {
         //properties will be visible, but uneditable
         function _prop(val) { return { value: val, enumerable: true }; }
 
@@ -25,20 +25,20 @@ var Sifter = (function() {
     }
 
     //printer filter properties (NOTE: uses single bit values so .keep() filters can just be flipped)
-    var _Types = {}, _OS = {};
-    Object.defineProperties(_Types, {
+    var Types = {}, OS = {};
+    Object.defineProperties(Types, {
         "PIXEL": { value: 1 },
         "RAW": { value: 2 },
         "BOTH": { value: 4 }
     });
-    Object.defineProperties(_OS, {
+    Object.defineProperties(OS, {
         "MAC": { value: 1 },
         "LINUX": { value: 2 },
         "WINDOWS": { value: 4 }
     });
 
     //usb device mapping
-    function _Vendor(name, type, vendor, product, device, endpoint) {
+    function Vendor(name, type, vendor, product, device, endpoint) {
         //properties will be visible, but uneditable
         function _prop(val) { return { value: val, enumerable: true }; }
 
@@ -52,78 +52,78 @@ var Sifter = (function() {
         });
     }
 
-    var _sift = {
+    var internal = {
 
         printDrivers: [
-            new _Driver('UNKNOWN', null, _Types.PIXEL, true),
+            new Driver('UNKNOWN', null, Types.PIXEL, true),
 
             /** Mac File Printers */
-            new _Driver('PDFwriter.PPD', _OS.MAC, _Types.PIXEL, false),
-            new _Driver('vipriser.PPD', _OS.MAC, _Types.PIXEL, false),
-            new _Driver('PSCOLOR.PPD', _OS.MAC, _Types.PIXEL, false),
+            new Driver('PDFwriter.PPD', OS.MAC, Types.PIXEL, false),
+            new Driver('vipriser.PPD', OS.MAC, Types.PIXEL, false),
+            new Driver('PSCOLOR.PPD', OS.MAC, Types.PIXEL, false),
 
             /** Linux File Printers */
-            new _Driver('CUPS-PDF.PPD', _OS.LINUX, _Types.PIXEL, false),
+            new Driver('CUPS-PDF.PPD', OS.LINUX, Types.PIXEL, false),
 
             /** Windows File Printers */
-            new _Driver('Microsoft XPS Document Writer', _OS.WINDOWS, _Types.PIXEL, false),
-            new _Driver('Microsoft Print To PDF', _OS.WINDOWS, _Types.PIXEL, false),
-            new _Driver('Send to Microsoft OneNote', _OS.WINDOWS, _Types.PIXEL, false),
-            new _Driver('PDFCreator', _OS.WINDOWS, _Types.PIXEL, false),
-            new _Driver('PrimoPDF', _OS.WINDOWS, _Types.PIXEL, false),
-            new _Driver('CutePDFWriter', _OS.WINDOWS, _Types.PIXEL, false),
-            new _Driver('Bullzip PDF', _OS.WINDOWS, _Types.PIXEL, false),
-            new _Driver('Adobe PDF', _OS.WINDOWS, _Types.PIXEL, false),
-            new _Driver('doPDF', _OS.WINDOWS, _Types.PIXEL, false),
-            new _Driver('novaPDF', _OS.WINDOWS, _Types.PIXEL, false),
-            new _Driver('OPTIsend', _OS.WINDOWS, _Types.PIXEL, false),
-            new _Driver('pdf995', _OS.WINDOWS, _Types.PIXEL, false),
-            new _Driver('docPrint PDF', _OS.WINDOWS, _Types.PIXEL, false),
-            new _Driver('EmfPrinter', _OS.WINDOWS, _Types.PIXEL, false),
-            new _Driver('ColorPlus', _OS.WINDOWS, _Types.PIXEL, false),
-            new _Driver('ImageRight', _OS.WINDOWS, _Types.PIXEL, false),
+            new Driver('Microsoft XPS Document Writer', OS.WINDOWS, Types.PIXEL, false),
+            new Driver('Microsoft Print To PDF', OS.WINDOWS, Types.PIXEL, false),
+            new Driver('Send to Microsoft OneNote', OS.WINDOWS, Types.PIXEL, false),
+            new Driver('PDFCreator', OS.WINDOWS, Types.PIXEL, false),
+            new Driver('PrimoPDF', OS.WINDOWS, Types.PIXEL, false),
+            new Driver('CutePDFWriter', OS.WINDOWS, Types.PIXEL, false),
+            new Driver('Bullzip PDF', OS.WINDOWS, Types.PIXEL, false),
+            new Driver('Adobe PDF', OS.WINDOWS, Types.PIXEL, false),
+            new Driver('doPDF', OS.WINDOWS, Types.PIXEL, false),
+            new Driver('novaPDF', OS.WINDOWS, Types.PIXEL, false),
+            new Driver('OPTIsend', OS.WINDOWS, Types.PIXEL, false),
+            new Driver('pdf995', OS.WINDOWS, Types.PIXEL, false),
+            new Driver('docPrint PDF', OS.WINDOWS, Types.PIXEL, false),
+            new Driver('EmfPrinter', OS.WINDOWS, Types.PIXEL, false),
+            new Driver('ColorPlus', OS.WINDOWS, Types.PIXEL, false),
+            new Driver('ImageRight', OS.WINDOWS, Types.PIXEL, false),
 
             /** Windows Raw-Only Printers **/
-            new _Driver('Generic / Text Only', _OS.WINDOWS, _Types.RAW, true),
+            new Driver('Generic / Text Only', OS.WINDOWS, Types.RAW, true),
 
             /** Windows Dual-Mode Printers **/
-            new _Driver('ZDesigner', _OS.WINDOWS, _Types.BOTH, true),
-            new _Driver('EPSON TM', _OS.WINDOWS, _Types.BOTH, true),
+            new Driver('ZDesigner', OS.WINDOWS, Types.BOTH, true),
+            new Driver('EPSON TM', OS.WINDOWS, Types.BOTH, true),
 
             /** Mac Raw-Only Printers */
-            new _Driver('TEXTONLY.PPD', _OS.MAC, _Types.RAW, true),
+            new Driver('TEXTONLY.PPD', OS.MAC, Types.RAW, true),
 
             /** Mac Dual-Mode Printers */
-            new _Driver('TM-T88V.PPD', _OS.MAC, _Types.BOTH, true),
+            new Driver('TM-T88V.PPD', OS.MAC, Types.BOTH, true),
 
             /** Linux Raw-Only Printers */
-            new _Driver('TEXTONLY.PPD', _OS.LINUX, _Types.RAW, true),
+            new Driver('TEXTONLY.PPD', OS.LINUX, Types.RAW, true),
 
             /** Linux Dual-Mode Printers */
-            new _Driver('EPTMBATH.PPD', _OS.LINUX, _Types.BOTH, true),
+            new Driver('EPTMBATH.PPD', OS.LINUX, Types.BOTH, true),
         ],
 
         usbVendors: [
 
             /** USB/HID Scales */
-            new _Vendor('Mettler Toledo', 'scale', '0x0EB8', '0xF000', '0x00', '0x81'),
-            new _Vendor('Dymo', 'scale', '0x0922', '0x8009', '0x00', '0x82'),
-            new _Vendor('Stamps.com', 'scale', '0x1446', '0x6A73', '0x00', '0x81'),
+            new Vendor('Mettler Toledo', 'scale', '0x0EB8', '0xF000', '0x00', '0x81'),
+            new Vendor('Dymo', 'scale', '0x0922', '0x8009', '0x00', '0x82'),
+            new Vendor('Stamps.com', 'scale', '0x1446', '0x6A73', '0x00', '0x81'),
 
         ],
 
         ///// PRIVATE METHODS /////
 
         findPrintDriver: function(driverName) {
-            for(var i = 0; i < _sift.printDrivers.length; i++) {
-                var driver = _sift.printDrivers[i];
+            for(var i = 0; i < internal.printDrivers.length; i++) {
+                var driver = internal.printDrivers[i];
                 if (driver.name.toUpperCase() === driverName.toUpperCase()
                         || driverName.toUpperCase().indexOf(driver.name.toUpperCase()) > -1) {
                     return driver;
                 }
             }
 
-            return _sift.printDrivers[0];
+            return internal.printDrivers[0];
         },
 
         findUsbVendor: function(vendorId, productId) {
@@ -135,7 +135,7 @@ var Sifter = (function() {
             type: function(list, param, value) {
                 for(var i = list.length - 1; i >= 0; i--) {
                     var item = list[i];
-                    var driver = _sift.findPrintDriver(item.driver);
+                    var driver = internal.findPrintDriver(item.driver);
 
                     if (driver && (driver[param] == value || (driver[param] & value) > 0)) {
                         list.splice(i, 1);
@@ -145,8 +145,8 @@ var Sifter = (function() {
 
             //look for printer style filters
             printers: function(list, filter) {
-                if (filter.type !== undefined) { _sift.filter.type(list, 'type', filter.type); }
-                if (filter.physical !== undefined) { _sift.filter.type(list, 'physical', filter.physical); }
+                if (filter.type !== undefined) { internal.filter.type(list, 'type', filter.type); }
+                if (filter.physical !== undefined) { internal.filter.type(list, 'physical', filter.physical); }
                 if (filter.os !== undefined) {
                     if (typeof filter.os === 'boolean') {
                         var appVersion = '';
@@ -156,11 +156,11 @@ var Sifter = (function() {
 
                             var found;
                             if (appVersion('Mac') != -1) {
-                                found = _OS.MAC;
+                                found = OS.MAC;
                             } else if (appVersion.indexOf('Linux') != -1 || appVersion.indexOf('X11') != -1) {
-                                found = _OS.LINUX;
+                                found = OS.LINUX;
                             } else {
-                                found = _OS.WINDOWS;
+                                found = OS.WINDOWS;
                             }
 
                             if (filter.os === false) { found = ~found; }
@@ -171,7 +171,7 @@ var Sifter = (function() {
                         }
                     }
 
-                    _sift.filter.type(list, 'os', filter.os);
+                    internal.filter.type(list, 'os', filter.os);
                 }
                 if (filter.name !== undefined) {
                     var regex = new RegExp(filter.name);
@@ -209,9 +209,9 @@ var Sifter = (function() {
             var alter = list.slice();
 
             if (alter[0].driver !== undefined) {
-                _sift.filter.printers(alter, filters);
+                internal.filter.printers(alter, filters);
             } else if (alter[0].vendor !== undefined) {
-                _sift.filter.usb(alter, filters);
+                internal.filter.usb(alter, filters);
             } else {
                 throw new Error("Cannot determine list's element type");
             }
@@ -272,34 +272,10 @@ var Sifter = (function() {
 
                 // Get status
                 scale.status.raw = parseInt(data[1], 16);
-                switch(scale.status.raw) {
-                    case 1: // fault
-                        scale.status.value = 'Fault';
-                        break;
-                    case 2: // stable at zero
-                        scale.status.value = 'Stable (Zero)';
-                        break;
-                    case 3: // busy
-                        scale.status.value = 'Busy';
-                        break;
-                    case 4: // stable non-zero
-                        scale.status.value = 'Stable';
-                        break;
-                    case 5: // underweight
-                        scale.status.value = 'Underweight';
-                        break;
-                    case 6: // overweight
-                        scale.status.value = 'Overweight';
-                        break;
-                    case 7: // calibrate
-                        scale.status.value = 'Calibrate';
-                        break;
-                    case 8: // re-zero
-                        scale.status.value = 'Tare';
-                        break;
-                    default:
-                        scale.status.value = 'Unknown';
-                        break;
+                var status = ['Unknown', 'Fault', 'Stable (Zero)', 'Busy', 'Stable', 'Underweight', 'Overweight', 'Calibrate', 'Tare'];
+                scale.status.value = status[scale.status.raw];
+                if (scale.status.value === undefined) {
+                    scale.status.value = 'Unknown';
                 }
 
                 // Get precision
@@ -340,14 +316,14 @@ var Sifter = (function() {
          * @param {boolean} active Whether debugging is enabled
          */
         debug: function(active) {
-            _sift.debug = active;
+            internal.debug = active;
         }
     };
 
     //Setup constants
     Object.defineProperties(sift, {
-        "Type": { value: _Types },
-        "OS": { value: _OS }
+        "Type": { value: Types },
+        "OS": { value: OS }
     });
 
     return sift;
